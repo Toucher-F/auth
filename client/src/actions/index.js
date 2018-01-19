1d55189539fe6c545f74136617388ad0a3ce729b
Toucher-F <lianduanb@gmail.com>
2018-01-19 14:15:21 -0500

    fetch message using jwt token to the backend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { browserHistory } from "react-router";
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from "./types";
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from "./types";
 
 const ROOT_URL = "http://localhost:3090";
 
@@ -50,5 +50,20 @@ export function signupUser({ email, password }) {
         browserHistory.push("/feature");
       })
       .catch(error => dispatch(authError(error.response.data.error)));
+  };
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios
+      .get(ROOT_URL, {
+        headers: { authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        });
+      });
   };
 }
--- client/src/components/feature.js
+++ client/src/components/feature.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class feature extends Component {
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
   render() {
-    return <div>This is a feature!</div>;
+    return <div>{this.props.message}</div>;
   }
 }
 
-export default feature;
+function mapStateToprops(state) {
+  return { message: state.auth.message };
+}
 
+export default connect(mapStateToprops, actions)(feature);
